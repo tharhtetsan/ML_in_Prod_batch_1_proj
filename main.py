9802b9261677e5d8c23ca8eefbfca07b49bf69e1
Thar Htet San <tharhtet1234@gmail.com>
2024-08-31 11:28:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 @app.get("/")
 def home():
     version = os.getenv("APP_VERSION")
-    str_out = "Hello world this is a test server version : {}".format(version)
+    dev_env = os.getenv("ENV")
+    str_out = "Hello world this is {} -  server version : {}".format(dev_env,version)
     return str_out
 
 if __name__== "__main__":
